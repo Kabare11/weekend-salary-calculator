7de4f91f672bc215fea6c9f4f3c93156e5185202
Abdulahi Kabare <abdullahikabare@gmail.com>
2023-12-03 17:12:32 -0600

    Cteated form inputs and the form function

--- calculator.js
+++ calculator.js
@@ -1,5 +1,17 @@
-console.log('JS FILE LOADED')
-function submitBtn(event) {
-    event.preventDefault()
-}
 
+console.log('JS FILE LOADED')
+
+const form = document.querySelector('form')
+
+function addNewDate(event) {
+    event.preventDefault();
+    const firstName = document.getElementById("firstNameInput").value;
+    const lastName = document.getElementById("lastNameInput").value;
+    const id = document.getElementById("idInput").value;
+    const title = document.getElementById("titleInput").value;
+    const annualsalary = document.getElementById("annualSalaryInput").value;
+    alert('firstname + lastname + id + title + annualsalary')
+
+    //alert('HAYEEY DOOLIGA')
+}
+form.addEventListener('submit', addNewDate)
